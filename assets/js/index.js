efdff9dc3df9e6a721fd9cc455a7c5bc6ecfe9f5
Joseph Moroni L. Azares <133185546+Jml-Azares@users.noreply.github.com>
2023-06-07 15:50:15 +0800

    added card functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,14 @@ function like(e) {
   e.classList.toggle("like");
   e.classList.toggle("fa-solid");
 }
+
+function read(e) {
+  e.classList.toggle("visible");
+}
+
+function remove(element) {
+  const card = element.closest(".myCard-cont");
+  if (card) {
+    card.remove();
+  }
+}
